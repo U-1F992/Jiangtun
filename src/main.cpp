296f5cfd382cafdf002ba3a9550b3f857abaebaa
Koutaro Mukai <mukai.k1011k@outlook.jp>
2023-09-21 18:21:27 +0900

    Update nxmc2_contrib

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,11 @@ static Gamecube_Data_t d = defaultGamecubeData;
 static const int SERIAL_INACTIVE_TIMEOUT = 100;
 static int inactive_count = 0;
 
-static NXMC2CommandBuilder builder;
-static NXMC2Command command;
-static NXMC2CommandHandler handler;
+static Nxmc2CommandBuilder builder;
+static Nxmc2Command command;
+static Nxmc2CommandHandler handler;
 
-static NXMC2CommandButtonState reset_state_ = NXMC2_COMMAND_BUTTON_STATE_RELEASED;
+static Nxmc2CommandButtonState reset_state_ = NXMC2_COMMAND_BUTTON_STATE_RELEASED;
 
 void setup()
 {
@@ -50,25 +50,25 @@ void setup()
     d.report.start = 0;
     console.write(d);
 
-    nxmc2_command_builder_initialize(&builder);
-    nxmc2_command_handler_initialize(&handler);
-    handler.y = [](NXMC2CommandButtonState state)
+    nxmc2_command_builder_new(&builder);
+    nxmc2_command_handler_new(&handler);
+    handler.y = [](Nxmc2CommandButtonState state)
     { d.report.y = (uint8_t)state; };
-    handler.b = [](NXMC2CommandButtonState state)
+    handler.b = [](Nxmc2CommandButtonState state)
     { d.report.b = (uint8_t)state; };
-    handler.a = [](NXMC2CommandButtonState state)
+    handler.a = [](Nxmc2CommandButtonState state)
     { d.report.a = (uint8_t)state; };
-    handler.x = [](NXMC2CommandButtonState state)
+    handler.x = [](Nxmc2CommandButtonState state)
     { d.report.x = (uint8_t)state; };
-    handler.l = [](NXMC2CommandButtonState state)
+    handler.l = [](Nxmc2CommandButtonState state)
     { d.report.l = (uint8_t)state; };
-    handler.r = [](NXMC2CommandButtonState state)
+    handler.r = [](Nxmc2CommandButtonState state)
     { d.report.r = (uint8_t)state; };
-    handler.zr = [](NXMC2CommandButtonState state)
+    handler.zr = [](Nxmc2CommandButtonState state)
     { d.report.z = (uint8_t)state; };
-    handler.plus = [](NXMC2CommandButtonState state)
+    handler.plus = [](Nxmc2CommandButtonState state)
     { d.report.start = (uint8_t)state; };
-    handler.home = [](NXMC2CommandButtonState state)
+    handler.home = [](Nxmc2CommandButtonState state)
     {
         if (reset_state_ == state)
         {
@@ -78,7 +78,7 @@ void setup()
         servo.write(state == NXMC2_COMMAND_BUTTON_STATE_PRESSED ? 65 : 90);
         reset_state_ = state;
     };
-    handler.hat = [](NXMC2CommandHatState state)
+    handler.hat = [](Nxmc2CommandHatState state)
     {
         switch (state)
         {
@@ -171,7 +171,7 @@ void static update_data()
     }
     inactive_count = 0;
 
-    NXMC2Result ret = nxmc2_command_builder_append(&builder, Serial.read());
+    Nxmc2Result ret = nxmc2_command_builder_append(&builder, Serial.read());
     if (ret != NXMC2_RESULT_OK)
     {
         nxmc2_command_builder_flush(&builder);
